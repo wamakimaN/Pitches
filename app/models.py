e84aa7ed2726ae23baa92559e27b6c7373ea1f57
wamakimaN <wamamoses@gmail.com>
2020-01-14 00:49:41 +0300

    finish up on login blueprint

--- app/models.py
+++ app/models.py
@@ -12,7 +12,19 @@ class User(UserMixin,db.Model):
     ideas = db.relationship('Idea',backref = 'owner',lazy='dynamic')
     profile_pic = db.Column(db.String(20),nullable = False,default = 'default.jpg')
 
-    
+    @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
+
+
 
     def __repr__(self):
         return f'User ("{self.username}","{self.profile_pic}")'
