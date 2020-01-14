90cee4f73ade5dea67df5fb84a7c21fc85eaaf66
wamakimaN <wamamoses@gmail.com>
2020-01-14 11:32:03 +0300

    completed user validation and signin

--- app/auth/form.py
+++ app/auth/form.py
@@ -4,9 +4,10 @@ from wtforms.validators import Required,Email,EqualTo
 from ..models import User
 
 class LoginForm(FlaskForm):
-    username = StringField('Username',validators=[Required(),Email()])
+    username = StringField('Username',validators=[Required()])
     pass_secure = PasswordField('Password',validators =[Required()])
     remember = BooleanField('Remember me')
+    submit = SubmitField('Sign In')
 
 class RegistrationForm(FlaskForm):
     email = StringField('Your Email Address',validators=[Required(),Email()])
--- app/auth/views.py
+++ app/auth/views.py
@@ -10,11 +10,13 @@ from .. import db
 def login():
 
     form = LoginForm()
+    if form.validate_on_submit():
+        return '<h1>' + form.username.data + ' ' + 'logged in' + '</h1>'
 
     return render_template('auth/login.html', form = form)
 
 @auth.route('/signup',methods = ["GET","POST"])
-def register():
+def signup():
     form = RegistrationForm()
     if form.validate_on_submit():
         user = User(email = form.email.data, username = form.username.data,pass_secure = form.pass_secure.data)
@@ -22,4 +24,4 @@ def register():
         db.session.commit()
         return redirect(url_for('auth.login'))
         title = "New Account"
-    return render_template('auth/signup.html',form = form)
+    return render_template('auth/signup.html',reg_form = form)
