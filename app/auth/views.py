d496d67c3855ff8279de018f71364fb01de7e3b5
wamakimaN <wamamoses@gmail.com>
2020-01-14 09:23:54 +0300

    created login form and its dependencies

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,6 +1,12 @@
 from flask import render_template
+from flask_wtf import FlaskForm
 from . import auth
+from .form import LoginForm
 
-@auth.route('/login')
+
+@auth.route('/login',methods = ['GET','POST'])
 def login():
-    return render_template('auth/login.html')
+
+    form = LoginForm()
+
+    return render_template('auth/login.html', form = form)
