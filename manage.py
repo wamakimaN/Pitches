f2e32b0a9ae6e8a048f6fa3adcc8b35fc642deca
wamakimaN <wamamoses@gmail.com>
2020-01-16 01:11:08 +0300

    re-deployment to heroku

--- manage.py
+++ manage.py
@@ -9,9 +9,12 @@ app = create_app('production')
 
 manager = Manager(app)
 migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
+
+
 
 manager.add_command('server',Server)
-manager.add_command('db',MigrateCommand)
+
 
 @manager.shell
 def make_shell_context():
