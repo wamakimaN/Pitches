b7d750054e56b219ccd7ab45e732ef3aa0a9fd32
wamakimaN <wamamoses@gmail.com>
2020-01-16 07:37:54 +0300

    another attempt on deployment

--- app/auth/form.py
+++ app/auth/form.py
@@ -4,7 +4,7 @@ from wtforms.validators import Required,Email,EqualTo
 from ..models import User
 
 class LoginForm(FlaskForm):
-    username = StringField('Username',validators=[Required()])
+    username = StringField('Email',validators=[Required()])
     password = PasswordField('Password',validators =[Required()])
     remember = BooleanField('Remember me')
     submit = SubmitField('Sign In')
