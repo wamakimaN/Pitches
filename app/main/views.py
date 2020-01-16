5723418d4d147c4c2cc6f5b032d21a7ee7f5a44b
wamakimaN <wamamoses@gmail.com>
2020-01-16 08:29:55 +0300

    change layout

--- app/main/views.py
+++ app/main/views.py
@@ -14,6 +14,8 @@ def index():
     View root page function that returns the index page and its data
     '''
 
+    
+
     return render_template('index.html')
 
 @main.route('/user/<uname>', methods = ['GET','POST'])
